c019021b262f3768b96ea4d0320f97cba574371d
Alvaro Fernandez <alvarofdayala@gmail.com>
2023-10-23 17:36:12 +0200

    deploy test docs

---
+++ docs/ball/ball.js
@@ -0,0 +1,1 @@
+export default class Ball{constructor(t,s){this.position={x:t,y:s},this.shape="b",this.speed=.1,this.collision_players=0,this.direc={x:Math.random()<=.5?-1:1,y:Math.random()<=.5?-1:1}}getY(){return this.position.y+this.shape.length+this.speed}getX(){return this.position.x+this.shape.length+this.speed}};
---
+++ docs/constans/constans.js
@@ -0,0 +1,1 @@
+export const BLOCK_SIZE=20;export const BOARD_WIDTH=40;export const BOARD_HEIGTH=14;export const PLAYERS_Y=BOARD_HEIGTH-4
