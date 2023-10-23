3aa34a29d2609b801dd087fa270e3ce0708d189d
Alvaro Fernandez <alvarofdayala@gmail.com>
2023-10-23 19:03:46 +0200

    music and reset

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import Game from "./game/game.js";
 
 window.addEventListener('load', () => {
+    playAudio();
+
     // init the game when page is loaded
     for(let b of document.getElementsByClassName('init')){
         b.addEventListener('click', e => {
             e.target.style.display = 'none';
             document.getElementsByClassName('winner')[0].style.display = 'none';
             new Game();
-            playAudio();
         })
     }
 
@@ -26,7 +27,29 @@ function start(e){
 }
 
 function playAudio(){
+    let playing_audio = false
+
     let audio = new Audio('music.mp3');
     audio.volume = 0.05;
-    audio.play();
+
+    let play_audio = document.getElementById('audio');
+    play_audio.addEventListener('click', () => {
+        playing_audio = !playing_audio;
+        if(playing_audio){
+            audio.play();
+            play_audio.innerHTML = 'Pausar audio';
+        }else{
+            audio.pause();
+            play_audio.innerHTML = 'Iniciar audio';
+        }
+    })
+
+    let reset_audio = document.getElementById('reset');
+    reset_audio.addEventListener('click', () => {
+        audio.pause();
+        audio = new Audio('music.mp3')
+        audio.volume = 0.05;
+        audio.play();
+        play_audio.innerHTML = 'Pausar audio';
+    })
 }
